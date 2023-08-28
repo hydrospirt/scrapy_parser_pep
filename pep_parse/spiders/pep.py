d636d9475348e4e4f9871c911c3054250e19bd35
Эдуард Гумен <hydrospirt@yandex.ru>
2023-08-28 17:32:07 +0300

    Add PepParseItem and fix func parse_pep

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,4 +1,5 @@
 import scrapy
+from pep_parse.items import PepParseItem
 
 
 class PepSpider(scrapy.Spider):
@@ -7,4 +8,18 @@ class PepSpider(scrapy.Spider):
     start_urls = ['http://peps.python.org/']
 
     def parse(self, response):
-        pass
+        pep_links = response.css('#numerical-index a::attr(href)').extract()
+        for pep_link in pep_links:
+            if pep_link and pep_link != '#numerical-index':
+                yield response.follow(pep_link, callback=self.parse_pep)
+
+    def parse_pep(self, response):
+        data = {
+            'number': int(response.css(
+                '#pep-content h1::text').re_first(r'\d+')),
+            'name': response.css(
+                '#pep-content h1::text').get().split('–')[1].strip(),
+            'status': response.css(
+                'dt:contains("Status") + dd abbr::text').get()
+        }
+        yield PepParseItem(data)
