c50d98df36eaf255921cb245288bcec1dd7a6a8e
Эдуард Гумен <hydrospirt@yandex.ru>
2023-08-30 11:53:34 +0300

    Fix OUTPUT_DIR and FILE_NAME

--- pep_parse/settings.py
+++ pep_parse/settings.py
@@ -10,11 +10,11 @@ ROBOTSTXT_OBEY = True
 ITEM_PIPELINES = {'pep_parse.pipelines.PepParsePipeline': 300, }
 BASE_DIR = Path(__file__).parent.parent
 DT_FORMAT = '%Y-%M-%d_%H-%M-%S'
-OUTPUT_DIR = BASE_DIR / 'results'
-FILE_NAME = 'status_summary_{time}.csv'
+OUTPUT_DIR = 'results'
+FILE_NAME = '{OUTPUT_DIR}/status_summary_{time}.csv'
 
 FEEDS = {
-    'results/pep_%(time)s.csv': {
+    f'{OUTPUT_DIR}/pep_%(time)s.csv': {
         'format': 'csv',
         'fields': ['number', 'name', 'status'],
         'owerwrite': True
